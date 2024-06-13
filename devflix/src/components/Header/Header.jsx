b69733c2679f13f8270531de64a240c18d7dd8c0
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2024-06-12 17:59:33 -0700

    Implemented a logo and further styling.

--- devflix/src/components/Header/Header.jsx
+++ devflix/src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 // src/components/Header/Header.jsx
 import React from 'react';
 import SearchForm from '../SearchForm/SearchForm';
-import './Header.css'
+import './Header.css';
+import logo from '../../images/1.png'; // Ensure the correct path
 
-function Header({ onSearchSubmit }) {  // Ensure this prop is received from the parent (App component)
+function Header({ onSearchSubmit }) {
     return (
         <header className="search-block">
             <a href="/" className="logo-link">
-                <h1>TheMovieDB</h1>
+                <div className="logo-container">
+                    <img src={logo} alt="Logo" className="logo-image" />
+                    <h1 className="site-title">DEVFLIX</h1>
+                </div>
             </a>
             <SearchForm onSearchSubmit={onSearchSubmit} /> 
         </header>
