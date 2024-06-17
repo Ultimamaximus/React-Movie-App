fdc51820c199ca1eaf3c4a961c5aabe4a9da72dd
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2024-06-16 19:17:10 -0700

    Created a sticky heading and reworked the info page.

--- devflix/src/components/Info/MovieInfoCard.jsx
+++ devflix/src/components/Info/MovieInfoCard.jsx
@@ -1,8 +1,6 @@
-// src/components/MovieInfoCard.jsx
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovies } from '../../services/api';
-
 import './MovieInfoCard.css';
 
 const MovieInfoCard = () => {
@@ -38,9 +36,16 @@ const MovieInfoCard = () => {
       <div className="movie-info-title">{movie.title}</div>
       <div className="movie-info-body">
         <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`${movie.title} poster`} className="movie-info-poster" />
-        <div className="movie-info-summary">{movie.overview}</div>
+        <div className="movie-info-details">
+          <div className="movie-info-summary">{movie.overview}</div>
+        </div>
       </div>
-      <div className="movie-info-rating">Rating: {movie.vote_average}</div>
+      <div className="movie-info-extra">
+        <div>Genre: {movie.genres.map(genre => genre.name).join(', ')}</div>
+        <div>Release Date: {movie.release_date}</div>
+        <div>Duration: {movie.runtime} minutes</div>
+      </div>
+      <div className="movie-info-rating">Rating: {movie.vote_average.toFixed(1)}</div>
     </div>
   );
 };
